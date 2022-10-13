0d21f5c7478b5a86d1acaad963625b0de7e354bf
palsiddharth091 <70902237+palsiddharth091@users.noreply.github.com>
2022-10-13 23:00:07 +0530

    Fixed Issue #18

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ $(function(){
         // DYNAMICALLY CREATE TIMEBLOCKS
         for(let i = 0; i < 5; i++){
             $("#timeBlocks").append(
-                `<div class='col-md-12 pb-2 mb-2 timeBlock' data-block='${i}' style='background: linear-gradient(#d3c8c854, black); border-radius:18px'>
+                `<div class='col-md-12 pb-2 my-3 mx-3  timeBlock' data-block='${i}' style='background: linear-gradient(#d3c8c854, black); border-radius:18px'>
                     <form class='form-inline m-2 p-2' id='activity-form'>
 
-                        <div class='col-xs-12 col-md-2'>
+                        
+<div class='hourdiv col-xs-12 col-md-2'>
                             <label for='inlineFormInputName2'>
-                                <h3 class='mt-2 p-1'>Hour: ${'0' + i}</h3>
+                                <h3 class='hour mt-2 p-1'>Hour: 0${i}</h3>
                             </label>
                         </div>
 
@@ -33,14 +34,15 @@ $(function(){
                 </div>`
             );   
         }
-        for(let i = 5; i < 8; i++){
+        for(let i = 5; i < 10; i++){
             $("#timeBlocks").append(
-                `<div class='col-md-12 pb-2 mb-2 timeBlock' data-block='${i}' style='background: linear-gradient(#c3b0a52b, #ff6c00); border-radius:18px'>
+                `<div class='col-md-12 pb-2 my-3 mx-3 timeBlock' data-block='${i}' style='background: linear-gradient(#c3b0a52b, #ff6c00); border-radius:18px'>
                     <form class='form-inline m-2 p-2' id='activity-form'>
 
-                        <div class='col-xs-12 col-md-2'>
+                        
+<div class='hourdiv col-xs-12 col-md-2'>
                             <label for='inlineFormInputName2'>
-                                <h3 class='mt-2 p-1'>Hour: ${'0' + i}</h3>
+                                <h3 class='hour mt-2 p-1'>Hour: 0${i}</h3>
                             </label>
                         </div>
 
@@ -57,14 +59,15 @@ $(function(){
                 </div>`
             );   
         }
-        for(let i = 8; i < 18; i++){
+        for(let i = 10; i < 18; i++){
             $("#timeBlocks").append(
-                `<div class='col-md-12 pb-2 mb-2 timeBlock' data-block='${i}' style='background: linear-gradient(#c9cbeb4f, #4848e0); border-radius:18px'>
+                `<div class='col-md-12 pb-2 my-3 mx-3 timeBlock' data-block='${i}' style='background: linear-gradient(#c9cbeb4f, #4848e0); border-radius:18px'>
                     <form class='form-inline m-2 p-2' id='activity-form'>
 
-                        <div class='col-xs-12 col-md-2'>
+                        
+<div class='hourdiv col-xs-12 col-md-2'>
                             <label for='inlineFormInputName2'>
-                                <h3 class='mt-2 p-1'>${i < 10 ? '0' + i : i}:00 Hr</h3>
+                                <h3 class='hour mt-2 p-1'>Hour: ${i}</h3>
                             </label>
                         </div>
 
@@ -83,12 +86,13 @@ $(function(){
         }
         for(let i = 18; i < 21; i++){
             $("#timeBlocks").append(
-                `<div class='col-md-12 pb-2 mb-2 timeBlock' data-block='${i}' style='background: linear-gradient(#c3b0a52b, #ff6c00); border-radius:18px'>
+                `<div class='col-md-12 pb-2 my-3 mx-3 timeBlock' data-block='${i}' style='background: linear-gradient(#c3b0a52b, #ff6c00); border-radius:18px'>
                     <form class='form-inline m-2 p-2' id='activity-form'>
 
-                        <div class='col-xs-12 col-md-2'>
+                        
+<div class='hourdiv col-xs-12 col-md-2'>
                             <label for='inlineFormInputName2'>
-                                <h3 class='mt-2 p-1'>Hour: ${i}</h3>
+                                <h3 class='hour mt-2 p-1'>Hour: ${i}</h3>
                             </label>
                         </div>
 
@@ -107,12 +111,12 @@ $(function(){
         }
         for(let i = 21; i < 24; i++){
             $("#timeBlocks").append(
-                `<div class='col-md-12 pb-2 mb-2 timeBlock' data-block='${i}' style='background: linear-gradient(#d3c8c854, black); border-radius:18px'>
+                `<div class='col-md-12 pb-2 my-3 mx-3 timeBlock' data-block='${i}' style='background: linear-gradient(#d3c8c854, black); border-radius:18px'>
                     <form class='form-inline m-2 p-2' id='activity-form'>
 
-                        <div class='col-xs-12 col-md-2'>
+                        <div class='hourdiv col-xs-12 col-md-2'>
                             <label for='inlineFormInputName2'>
-                                <h3 class='mt-2 p-1'>Hour: ${i}</h3>
+                                <h3 class='hour mt-2 p-1'>Hour: ${i}</h3>
                             </label>
                         </div>
 
